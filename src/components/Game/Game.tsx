2bccb8a3f99073634d971f48bd0ecbff02c6b2e4
Katya Pavlenko <kpavlenko@yotpo.com>
2023-04-29 19:31:46 +0300

    fix scrolling

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -8,24 +8,26 @@ export function Game() {
     const [currentRoom, setCurrentRoom] = React.useState<RoomName>(RoomName.living);
     const [heroPosition, setHeroPosition] = React.useState<{ left: number, top: number }>({ left: 0, top: 0 });
     const [debugMode, setDebugMode] = React.useState<boolean>(false);
-
+    const [scale, setScale] = React.useState(5);
     const moveToRoom = (room: RoomName) => () => {
         setCurrentRoom(room);
     };
 
     useEffect(() => {
         const { left, top, width, height } = rooms[currentRoom];
-        setHeroPosition({ left: left + width / 2 - 20, top: top + height / 2 - 20 });
+        setHeroPosition({ left: left*scale + width * scale / 2 - 20, top: top*scale  + height*scale / 2 - 20 });
     }, [currentRoom]);
 
     return (
         <div className={styles.game}>
             {/*<img className={styles.bg} src={suburb}/>*/}
-            <House className={styles.house}/>
-            {Object.keys(rooms).map((room: RoomName) => {
-                const { left, top, width, height } = rooms[room];
-                return <div key={room} className={styles.roomDummie} style={{ left, top, width, height, display: debugMode ? 'block': 'none' }} onClick={moveToRoom(room)}/>;
-            })}
+            <House scale={scale}
+                   setScale={setScale}
+                   className={styles.house} setCurrentRoom={setCurrentRoom} currentRoom={currentRoom}/>
+            {/*{Object.keys(rooms).map((room: RoomName) => {*/}
+            {/*    const { left, top, width, height } = rooms[room];*/}
+            {/*    return <div key={room} className={styles.roomDummie} style={{ left, top, width, height, display: debugMode ? 'block': 'none' }} onClick={moveToRoom(room)}/>;*/}
+            {/*})}*/}
             <Hero style={{ left: heroPosition.left, top: heroPosition.top }}/>
 
         </div>
