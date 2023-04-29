a029fcaa89facf08061e7c5c21873ece5639669b
Katya Pavlenko <kpavlenko@yotpo.com>
2023-04-29 13:45:27 +0300

    add letter

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Game } from '../Game/Game';
+import { Letter } from '../Letter/Letter';
 import styles from './App.module.scss';
 
 function App() {
+    const [showMenu, setShowMenu] = React.useState<boolean>(true);
+    const menuClick = () => {
+        setShowMenu(false);
+    };
     return (
         <div className={styles.App}>
-            <h1 className={styles.title}> Game</h1>
-            <Game/>
+            {/*<h1 className={styles.title}> Game</h1>*/}
+            {showMenu ? <Letter onClick={() => menuClick()}/> : <Game/>}
         </div>
 
     );
--- src/components/Letter/Letter.tsx
+++ src/components/Letter/Letter.tsx
@@ -3,10 +3,17 @@ import React, { useEffect } from 'react';
 import styles from './Letter.module.scss';
 
 
-export function Letter() {
+export function Letter({...rest}) {
 
     return (
-        <div className={styles.letter}>
+        <div className={styles.letter} {...rest} >
+            <div className={styles.bg}></div>
+            <span className={styles.text}>
+            Hi, I'm a letter!
+            There's something strange in your neighborhood.
+            Go and check it....
+            This font gonna be changed, dont worry
+                </span>
 
         </div>
 
