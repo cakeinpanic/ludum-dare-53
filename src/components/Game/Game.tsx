32d8e9049e13eea01d3909cfb79823557d45d0c9
Katya Pavlenko <kpavlenko@yotpo.com>
2023-04-29 11:45:23 +0300

    add moving between rooms

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,16 +1,61 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Hero } from '../Hero/Hero';
 import { House } from '../House/House';
 import styles from './Game.module.scss';
 
+enum RoomName {
+    attic = 'attic',
+    toilet = 'toilet',
+    cabinet = 'cabinet'
+}
+
+const rooms = {
+    [RoomName.attic]: {
+        left: 72,
+        top: 103,
+        width: 350,
+        height: 125
+    },
+    [RoomName.toilet]: {
+        left: 72,
+        top: 232,
+        width: 93,
+        height: 105,
+    },
+    [RoomName.cabinet]: {
+        left: 162,
+        top: 232,
+        width: 258,
+        height: 105,
+    },
+};
+
 export function Game() {
+    const [currentRoom, setCurrentRoom] = React.useState<RoomName>(RoomName.attic);
+    const [heroPosition, setHeroPosition] = React.useState<{ left: number, top: number }>({ left: 0, top: 0 });
+    const [debugMode, setDebugMode] = React.useState<boolean>(false);
+
+    const moveToRoom = (room: RoomName) => () => {
+        setCurrentRoom(room);
+    };
+
+    useEffect(() => {
+        const { left, top, width, height } = rooms[currentRoom];
+        setHeroPosition({ left: left + width / 2 - 20, top: top + height / 2 - 20 });
+    }, [currentRoom]);
+
     return (
         <div className={styles.game}>
-            <House className={styles.house} />
-            <Hero/>
+            <House className={styles.house}/>
+            {Object.keys(rooms).map((room: RoomName) => {
+                const { left, top, width, height } = rooms[room];
+                return <div key={room} className={styles.roomDummie} style={{ left, top, width, height, opacity: debugMode ? .5: 0 }} onClick={moveToRoom(room)}/>;
+            })}
+            <Hero style={{ left: heroPosition.left, top: heroPosition.top }}/>
         </div>
 
     );
-};
+}
+;
 
 
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { ComponentProps } from 'react';
 import styles from './Hero.module.scss';
 import { SpriteAnimator } from 'react-sprite-animator'
 import bird from './bird.png'
-export function Hero() {
+export function Hero({...props}: ComponentProps<any>) {
     return (
-        <div className={styles.Hero}>
+        <div className={styles.Hero} style={props.style}>
             <SpriteAnimator
                 sprite={bird}
                 fps={10}
