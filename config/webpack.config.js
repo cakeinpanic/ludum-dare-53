07e3154a9744f2b35fb9f07ecac42b7b3284f708
Katya Pavlenko <kpavlenko@yotpo.com>
2023-04-30 00:17:50 +0300

    turn off optimizaiton plug, it takes toooo logn

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -250,43 +250,43 @@ module.exports = function (webpackEnv) {
     optimization: {
       minimize: isEnvProduction,
       minimizer: [
-        new ImageMinimizerPlugin({
-          minimizer: {
-            implementation: ImageMinimizerPlugin.imageminMinify,
-            options: {
-              // Lossless optimization with custom option
-              // Feel free to experiment with options for better result for you
-              plugins: [
-                //["gifsicle", { interlaced: true }],
-                //["jpegtran", { progressive: true }],
-                ["optipng", { optimizationLevel: 5 }],
-                // Svgo configuration here https://github.com/svg/svgo#configuration
-                [
-                  "svgo",
-                  {
-                    plugins: [
-                      {
-                        name: "preset-default",
-                        params: {
-                          overrides: {
-                            removeViewBox: false,
-                            addAttributesToSVGElement: {
-                              params: {
-                                attributes: [
-                                  { xmlns: "http://www.w3.org/2000/svg" },
-                                ],
-                              },
-                            },
-                          },
-                        },
-                      },
-                    ],
-                  },
-                ],
-              ],
-            },
-          },
-        }),
+        //new ImageMinimizerPlugin({
+        //  minimizer: {
+        //    implementation: ImageMinimizerPlugin.imageminMinify,
+        //    options: {
+        //      // Lossless optimization with custom option
+        //      // Feel free to experiment with options for better result for you
+        //      plugins: [
+        //        //["gifsicle", { interlaced: true }],
+        //        //["jpegtran", { progressive: true }],
+        //        ["optipng", { optimizationLevel: 5 }],
+        //        // Svgo configuration here https://github.com/svg/svgo#configuration
+        //        [
+        //          "svgo",
+        //          {
+        //            plugins: [
+        //              {
+        //                name: "preset-default",
+        //                params: {
+        //                  overrides: {
+        //                    removeViewBox: false,
+        //                    addAttributesToSVGElement: {
+        //                      params: {
+        //                        attributes: [
+        //                          { xmlns: "http://www.w3.org/2000/svg" },
+        //                        ],
+        //                      },
+        //                    },
+        //                  },
+        //                },
+        //              },
+        //            ],
+        //          },
+        //        ],
+        //      ],
+        //    },
+        //  },
+        //}),
         // This is only used in production mode
         new TerserPlugin({
           terserOptions: {
