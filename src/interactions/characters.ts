4a8995fd95650def50c6ba26cdd966c97b9b7404
Alexey Kalmakov (rndD) <st00nsa@gmail.com>
2023-05-01 16:36:25 +0200

    text with ma without letter

--- src/interactions/characters.ts
+++ src/interactions/characters.ts
@@ -4,63 +4,66 @@ import {
   Item,
   ItemName,
   ItemsCollection,
-} from "../components/Game/types";
-import { RoomName } from "../rooms/rooms";
-import { getText } from "./texts";
-import { InteractionResult } from "./types";
+} from '../components/Game/types'
+import { RoomName } from '../rooms/rooms'
+import { getText } from './texts'
+import { InteractionResult } from './types'
 
 export const clickOnCharacterInteraction = (
   characterName: CharacterName,
   items: ItemsCollection,
   characters: CharactersCollection,
   currentItem: Item,
-  act: number
+  act: number,
 ): InteractionResult => {
   const result: InteractionResult = {
     newCurrentItem: currentItem,
     updateItemsObject: {},
     updateCharactersObject: {},
     newHelpText: null,
-  };
-  if (characterName === "ma") {
-    if (currentItem?.id === "flowers") {
-      return giveFlowersToMother(items, characters, currentItem);
-    }
-    return talkToMother(items, characters, currentItem, act);
   }
-  if (characterName === "sister") {
-    if (currentItem?.id === "letter") {
-      return talkToSisterAboutLetter(items, characters, currentItem);
+  if (characterName === 'ma') {
+    if (currentItem?.id === 'flowers') {
+      return giveFlowersToMother(items, characters, currentItem)
     }
-    return talkToSister(items, characters, currentItem, act);
+    if (currentItem?.id === 'letter') {
+      return talkToMotherAboutLetter(items, characters, currentItem, act)
+    }
+    return talkToMother(items, characters, currentItem, act)
+  }
+  if (characterName === 'sister') {
+    if (currentItem?.id === 'letter') {
+      return talkToSisterAboutLetter(items, characters, currentItem)
+    }
+    return talkToSister(items, characters, currentItem, act)
   }
 
   if (characterName === CharacterName.ghost) {
-    if (currentItem?.id === "photo") {
-      return showPhotoToGhost(items, characters, currentItem, act);
+    if (currentItem?.id === 'photo') {
+      return showPhotoToGhost(items, characters, currentItem, act)
     }
-    return talkToGhost(items, characters, currentItem, act);
+    return talkToGhost(items, characters, currentItem, act)
   }
-  return result;
-};
+  return result
+}
 
 const talkToSisterAboutLetter = (
   items: ItemsCollection,
   characters: CharactersCollection,
-  currentItem: Item
+  currentItem: Item,
 ): InteractionResult => {
   return {
     newCurrentItem: items[ItemName.scissors],
     updateItemsObject: {},
     updateCharactersObject: {},
-    newHelpText: getText("2"),
-  };
-};
+    newHelpText: getText('2'),
+  }
+}
 
 const giveFlowersToMother = (
   items: ItemsCollection,
   characters: CharactersCollection,
-  currentItem: Item
+  currentItem: Item,
 ): InteractionResult => {
   return {
     newCurrentItem: null,
@@ -78,37 +81,51 @@ const giveFlowersToMother = (
       [ItemName.vase]: { ...items[ItemName.vase], isActive: false },
     },
     updateCharactersObject: {
-      ["ma"]: {
-        ...characters["ma"],
+      ma: {
+        ...characters['ma'],
         room: RoomName.kitchen,
         roomPosition: { shiftX: -20, shiftY: 0 },
       },
     },
-    newHelpText: getText("3"),
+    newHelpText: getText('3'),
     nextAct: true,
     updatedStatus: { upstairsIsBlockedByMa: false },
-  };
-};
+  }
+}
+
+const talkToMotherAboutLetter = (
+  items: ItemsCollection,
+  characters: CharactersCollection,
+  currentItem: Item,
+  act: number,
+): InteractionResult => {
+  return {
+    newCurrentItem: currentItem,
+    updateItemsObject: {},
+    updateCharactersObject: {},
+    newHelpText: getText('1'),
+  }
+}
 
 const talkToMother = (
   items: ItemsCollection,
   characters: CharactersCollection,
   currentItem: Item,
-  act: number
+  act: number,
 ): InteractionResult => {
   return {
     newCurrentItem: currentItem,
     updateItemsObject: {},
     updateCharactersObject: {},
-    newHelpText: getText("1"),
-  };
-};
+    newHelpText: getText('7'),
+  }
+}
 
 const talkToSister = (
   items: ItemsCollection,
   characters: CharactersCollection,
   currentItem: Item,
-  act: number
+  act: number,
 ): InteractionResult => {
   return {
     newCurrentItem: currentItem,
@@ -116,15 +133,15 @@ const talkToSister = (
     updateCharactersObject: {},
     newHelpText:
       act === 3
-        ? "Sister: you did not spend enough time with us today, go talk to dad"
-        : getText("4"),
-  };
-};
+        ? 'Sister: you did not spend enough time with us today, go talk to dad'
+        : getText('4'),
+  }
+}
 const talkToGhost = (
   items: ItemsCollection,
   characters: CharactersCollection,
   currentItem: Item,
-  act: number
+  act: number,
 ): InteractionResult => {
   return {
     newCurrentItem: currentItem,
@@ -132,16 +149,16 @@ const talkToGhost = (
     updateCharactersObject: {},
     newHelpText:
       act === 3
-        ? "Ghost: i would help you"
+        ? 'Ghost: i would help you'
         : "Ghost: there is a dark secret i'm gonna show you",
-  };
-};
+  }
+}
 
 const showPhotoToGhost = (
   items: ItemsCollection,
   characters: CharactersCollection,
   currentItem: Item,
-  act: number
+  act: number,
 ): InteractionResult => {
   return {
     newCurrentItem: null,
@@ -150,5 +167,5 @@ const showPhotoToGhost = (
     newHelpText:
       "Look at this photo, you see where it's taken? It's on the attic, let me show you the way upstairs",
     updatedStatus: { atticIsHidden: false },
-  };
-};
+  }
+}
