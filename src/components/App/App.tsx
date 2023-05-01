e8d31bee6eb4f608752a5bb085690d0831900403
Katya Pavlenko <kpavlenko@yotpo.com>
2023-05-02 00:31:51 +0300

    add nicer preloader

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -55,7 +55,7 @@ function App() {
   return (
     <div className={styles.App}>
       {showMenu ? (
-        <Letter onClick={() => menuClick()} />
+        <Letter onExit={() => menuClick()} />
       ) : (
         <>
           <Game gameProps={gameProps} />
--- src/components/Letter/Letter.tsx
+++ src/components/Letter/Letter.tsx
@@ -2,21 +2,56 @@ import React from "react";
 
 import styles from "./Letter.module.scss";
 
-export function Letter({ ...rest }) {
+export function Letter({ onExit, ...rest }) {
+  const [showPreText, setShowPreText] = React.useState<boolean>(false);
+  const [showTitle, setShowTitle] = React.useState<boolean>(true);
+  const [showLetter, setShowLetter] = React.useState<boolean>(false);
   return (
     <div className={styles.letter} {...rest}>
-      <div className={styles.container}>
-        <div className={styles.bg}></div>
-        <span className={styles.text}>
-          Hi! <br />
-          <span>
-            Writing you in a rush – there's something weird going in your
-            parents house, please come and check it out ASAP.
+      {showTitle && (
+        <div
+          onClick={() => {
+            setShowTitle(false);
+            setShowPreText(true);
+          }}
+        >
+          <div className={styles.title}>Return to sender</div>
+          <a
+            className={styles.subtitle}
+            href="https://ldjam.com/events/ludum-dare/53/"
+          >
+            ldjam 53 game
+          </a>
+        </div>
+      )}
+      {showPreText && (
+        <div
+          onClick={() => {
+            setShowPreText(false);
+            setShowLetter(true);
+          }}
+        >
+          <div className={styles.subtitle}>
+            Yesterday you got a very strange letter in your mailbox, you don't
+            know who sent it, and it does not contain a lot of information
+            neither...
+          </div>
+        </div>
+      )}
+      {showLetter && (
+        <div className={styles.container} onClick={() => onExit()}>
+          <div className={styles.bg}></div>
+          <span className={styles.text}>
+            Hi! <br />
+            <span>
+              Writing you in a rush – there's something weird going in your
+              parents house, please come and check it out ASAP.
+            </span>
+            <br />
+            <span className={styles.signature}>Love, J</span>
           </span>
-          <br />
-          <span className={styles.signature}>Love, J</span>
-        </span>
-      </div>
+        </div>
+      )}
     </div>
   );
 }
