f8888bb5856e40d86d9e6e24110ab3e3313ef6a2
Alexey Kalmakov (rndD) <st00nsa@gmail.com>
2023-05-02 00:41:54 +0200

    more fixes texts

--- src/interactions/texts.ts
+++ src/interactions/texts.ts
@@ -8,7 +8,7 @@ export const texts = {
   // Ma with flowers
   "3": "Mother: Thanks my dear, your self-sacrifice... I said to others that you are here | Mother went to put flowers in the vase",
   // Talk to sister without letter
-  "4": "Sister: I'm so excited about the todays event, you will a star of the show.",
+  "4": "Sister: I'm so excited about the todays event, you will be the star of the show",
   // vase without flowers
   "5": `Flowers from the garden would look good in this vase`,
   // Tree
