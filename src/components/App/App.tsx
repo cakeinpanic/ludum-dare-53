eb78cad949adb68ba2d1aa11c89829efe7da331f
Katya Pavlenko <kpavlenko@yotpo.com>
2023-05-02 10:06:52 +0300

    fix ratio

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -42,7 +42,7 @@ function App() {
   const updateSize = () => {
     console.log(window.innerWidth);
     if (window.innerWidth > 1000) {
-      setStyle({ transform: `scale(1)` });
+      setStyle({});
       return;
     }
     const ratio = (window.innerWidth - 200) / 1000;
